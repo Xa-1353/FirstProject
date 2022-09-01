56376a549104694a41be008f155fd3890599e114
Xaria A <xaria.shella@gmail.com>
2022-09-01 09:20:24 -0500

    added another line of print

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -1,6 +1,7 @@
 public class MyFirstClass {
     public static void main(String[] args) {
-        System.out.print("tuesday");
+        System.out.print("tuesday  ");
         System.out.println("8.30");
+        System.out.println("ryan the duck");
     }
 }
